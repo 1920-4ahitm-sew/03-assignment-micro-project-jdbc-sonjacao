da2556d20c8633b06c8829696b50ef4cfcc7a494
Sonja Cao <csonja1703@gmail.com>
2019-10-19 12:41:40 +0200

    Created class Customer

---
+++ LibraryEE/src/main/java/at/htl/library/model/Customer.java
@@ -0,0 +1,26 @@
+package at.htl.library.model;
+
+import java.time.LocalDateTime;
+
+public class Customer extends Person{
+
+    private Long customerId;
+
+    public Customer( Long customerId, Long ssno, String firstName, String lastName, LocalDateTime dateOfBirth, String gender, String street, Long streetNo, Long postalCode, String city, Long phoneNo, String email) {
+        super(ssno, firstName, lastName, dateOfBirth, gender, street, streetNo, postalCode, city, phoneNo, email);
+        this.customerId = customerId;
+    }
+
+    public Customer() {
+    }
+
+    // Getter for customer id
+    public Long getCustomerId() {
+        return customerId;
+    }
+    // Setter for customer id
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+}
