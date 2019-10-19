1565009fbd783a25fc55cd29ef6f97d7903a2033
Sonja Cao <csonja1703@gmail.com>
2019-10-19 12:41:16 +0200

    Created class Author

---
+++ LibraryEE/src/main/java/at/htl/library/model/Author.java
@@ -0,0 +1,39 @@
+package at.htl.library.model;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Author extends Person {
+
+    private Long authorNo;
+    private List<Book> publications = new LinkedList<>();
+
+    public Author(Long authorNo, List<Book> publications, Long ssno, String firstName, String lastName, LocalDateTime dateOfBirth, String gender, String street, Long streetNo, Long postalCode, String city, Long phoneNo, String email) {
+        super(ssno, firstName, lastName, dateOfBirth, gender, street, streetNo, postalCode, city, phoneNo, email);
+        this.authorNo = authorNo;
+        this.publications = publications;
+    }
+
+    public Author() {
+    }
+
+    //region Getter and Setter
+    public Long getAuthorNo() {
+        return authorNo;
+    }
+
+    public void setAuthorNo(Long authorNo) {
+        this.authorNo = authorNo;
+    }
+
+    public List<Book> getPublications() {
+        return publications;
+    }
+
+    public void setPublications(List<Book> publications) {
+        this.publications = publications;
+    }
+    //endregion
+
+}
