f1be794aa3bcb07e02dd31ee00997e33c58532fe
Sonja Cao <csonja1703@gmail.com>
2019-10-19 12:40:52 +0200

    Created class Person

---
+++ LibraryEE/src/main/java/at/htl/library/model/Person.java
@@ -0,0 +1,131 @@
+package at.htl.library.model;
+
+import java.time.LocalDateTime;
+
+public class Person {
+
+    private Long ssno;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime dateOfBirth;
+    private String gender;
+    private String street;
+    private Long streetNo;
+    private Long postalCode;
+    private String city;
+    private Long phoneNo;
+    private String email;
+
+    public Person(Long ssno, String firstName, String lastName, LocalDateTime dateOfBirth, String gender, String street, Long streetNo, Long postalCode, String city, Long phoneNo, String email) {
+        this.ssno = ssno;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.street = street;
+        this.streetNo = streetNo;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+
+    public Person() {
+    }
+
+    //region Getter and Setter
+    public Long getSsno() {
+        return ssno;
+    }
+
+    public void setSsno(Long ssno) {
+        this.ssno = ssno;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Long getStreetNo() {
+        return streetNo;
+    }
+
+    public void setStreetNo(Long streetNo) {
+        this.streetNo = streetNo;
+    }
+
+    public Long getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Long postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(Long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.firstName, this.lastName);
+    }
+}
