23833c62a60f4c3442027739169b0b9dea0878fc
Sonja Cao <csonja1703@gmail.com>
2019-10-22 20:23:25 +0200

    Modified method doGet so it displays the content of the database table publishing_house

--- LibraryEE/src/main/java/at/htl/library/servlet/LibraryServlet.java
+++ LibraryEE/src/main/java/at/htl/library/servlet/LibraryServlet.java
@@ -1,17 +1,56 @@
 package at.htl.library.servlet;
 
+import at.htl.library.database.Database;
+import at.htl.library.model.PublishingHouse;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("libservlet")
 public class LibraryServlet extends HttpServlet {
 
+    static Database db = new Database();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        PrintWriter output = resp.getWriter();
+        resp.setContentType("text/html");
+        output.println("<html>");
+        output.println("<head>" +
+                "<title>Publishing Houses</title>" +
+                "<meta charset='UTF-8'>" +
+                "</head>");
+        output.println("<body>");
+        output.println("<h1>Publishing Houses</h1>");
+        output.println("<table>");
+        output.println("<tr>" +
+                "<th>Publisher No.</th>" +
+                "<th>Publisher Name</th>" +
+                "<th>Street</th>" +
+                "<th>Postal Code</th>" +
+                "<th>City</th>" +
+                "<th>Country</th>" +
+                "</tr>");
+
+        List<PublishingHouse> publishingHouseList = db.selectAllPublishingHouses();
+        for (PublishingHouse ph : publishingHouseList) {
+            output.println("<tr>" +
+                    "<td>" + ph.getPublisherNo() + "</td>" +
+                    "<td>" + ph.getPublisherName() + "</td>" +
+                    "<td>" + ph.getStreet() + "</td>" +
+                    "<td>" + ph.getPostalCode() + "</td>" +
+                    "<td>" + ph.getCity() + "</td>" +
+                    "<td>" + ph.getCountry() + "</td>" +
+                    "</tr>");
+        }
+
+        output.println("</table>");
+        output.println("</body></html>");
     }
 }
